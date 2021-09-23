e8f3e52f6b4d6bae59fd0431aa8d42bcc4f13944
David Benn <David.Benn@csiro.au>
2021-09-23 12:05:25 +0930

    added format specific functions and a common reader plus UTs

--- R/read_soilspec.R
+++ R/read_soilspec.R
@@ -1,4 +1,4 @@
-# Read a soil spectroscopy file
+# Functions to read a soil spectroscopy file
 
 source("R/spectrum_format.R", local=T)
 source("R/nicolet_spa_format.R", local=T)
@@ -12,7 +12,7 @@ soilspec.handlers[[".spc"]] <- ThermoSpc$new()
 soilspec.handlers[[".0"]] <- BrukerOpusBinary$new()
 soilspec.handlers[[".asd"]] <- ASDBinary$new()
 
-#' Read a soil spectroscopy file given a path
+#' Read a soil spectroscopy file given only its path
 #' @export
 #' @param path Full path to the file
 #' @return A result list containing a file read status, a data.frame of
@@ -23,7 +23,7 @@ soilspec.handlers[[".asd"]] <- ASDBinary$new()
 read.soilspec <- function(path) {
 
   result <- NULL
-  
+
   if (!file.exists(path)) {
     status <- 1
   } else {
@@ -35,6 +35,103 @@ read.soilspec <- function(path) {
       } else {
         status <- 2
       }
+    } else {
+      status <- 2
+    }
+  }
+
+  if (is.null(result)) {
+    result <- list()
+    result[["status"]] <- status
+    result[["mode"]] <- NULL
+    result[["units"]] <- NULL
+    result[["is.descending"]] <- NULL
+    result[["data"]] <- NULL
+    result[["metadata"]] <- NULL
+  }
+
+  result
+}
+
+#' Read a Nicolet spa soil spectroscopy file given a path.
+#' A precondition for correct functioning is that the file is of the expected type.
+#' This function should be used when the file does not have the expected ".spa" suffix.
+#' @export
+#' @param path Full path to the file
+#' @return A result list containing a file read status, a data.frame of
+#'         wavenumber-intensity pairs, a list of any available metadata,
+#'         instrument mode, units, whether wavenumbers are in descending order;
+#'         status will be non-zero if file does not exist or cannot be read (1),
+#'         is of an unknown format (2) or some other error occurred (4).
+read.nicolet.spa <- function(path) {
+
+  read.soilspec.common(path, ".spa")
+}
+
+#' Read a Bruker Opus Binary soil spectroscopy file given a path.
+#' A precondition for correct functioning is that the file is of the expected type.
+#' This function should be used when the file does not have the expected ".0" suffix.
+#' @export
+#' @param path Full path to the file
+#' @return A result list containing a file read status, a data.frame of
+#'         wavenumber-intensity pairs, a list of any available metadata,
+#'         instrument mode, units, whether wavenumbers are in descending order;
+#'         status will be non-zero if file does not exist or cannot be read (1),
+#'         is of an unknown format (2) or some other error occurred (4).
+read.bruker.opus.binary <- function(path) {
+
+  read.soilspec.common(path, ".0")
+}
+
+#' Read an ASD Binary soil spectroscopy file given a path.
+#' A precondition for correct functioning is that the file is of the expected type.
+#' This function should be used when the file does not have the expected ".asd" suffix.
+#' @export
+#' @param path Full path to the file
+#' @return A result list containing a file read status, a data.frame of
+#'         wavenumber-intensity pairs, a list of any available metadata,
+#'         instrument mode, units, whether wavenumbers are in descending order;
+#'         status will be non-zero if file does not exist or cannot be read (1),
+#'         is of an unknown format (2) or some other error occurred (4).
+read.asd.binary <- function(path) {
+
+  read.soilspec.common(path, ".asd")
+}
+
+#' Read an Thermo spc soil spectroscopy file given a path.
+#' A precondition for correct functioning is that the file is of the expected type.
+#' This function should be used when the file does not have the expected ".spc" suffix.
+#' @export
+#' @param path Full path to the file
+#' @return A result list containing a file read status, a data.frame of
+#'         wavenumber-intensity pairs, a list of any available metadata,
+#'         instrument mode, units, whether wavenumbers are in descending order;
+#'         status will be non-zero if file does not exist or cannot be read (1),
+#'         is of an unknown format (2) or some other error occurred (4).
+read.thermo.spc <- function(path) {
+
+  read.soilspec.common(path, ".spc")
+}
+
+
+#' Common soil spectroscopy file reader function.
+#' A precondition for correct functioning is that the file and reader are compatible.
+#' @param path Full path to the file
+#' @param assumed.suffix The assumed suffix for the expected file format
+#' @return A result list containing a file read status, a data.frame of
+#'         wavenumber-intensity pairs, a list of any available metadata,
+#'         instrument mode, units, whether wavenumbers are in descending order;
+#'         status will be non-zero if file does not exist or cannot be read (1),
+#'         is of an unknown format (2) or some other error occurred (4).
+read.soilspec.common <- function(path, assumed.suffix) {
+
+  result <- NULL
+
+  if (!file.exists(path)) {
+    status <- 1
+  } else {
+    if (assumed.suffix %in% names(soilspec.handlers)) {
+      result <- soilspec.handlers[[assumed.suffix]]$read(path)
     } else {
       status <- 2
     }
--- tests/testthat/test_read_soilspec.R
+++ tests/testthat/test_read_soilspec.R
@@ -1,15 +1,15 @@
 # Unit tests for generic read function (read.soilspec)
 
-# Tests for each known type. We check a subset of individual R6 class tests.
+# Tests for each known type via generic read function
 
 test_that("Read ASD binary example file with generic read function", {
   path <- soilspec.format::asd.binary.file.path()
   result <- soilspec.format::read.soilspec(path)
-  
+
   testthat::expect_equal(object = result$status, expected = 0)
 
   testthat::expect_false(result$is.descending)
-  
+
   testthat::expect_equal(object = nrow(result$data),
                          expected = 2151)
 })
@@ -17,15 +17,15 @@ test_that("Read ASD binary example file with generic read function", {
 test_that("Read Bruker Opus binary example file with generic read function", {
   path <- soilspec.format::bruker.opus.binary.file.path()
   result <- soilspec.format::read.soilspec(path)
-  
+
   testthat::expect_equal(object = result$status, expected = 0)
-  
+
   testthat::expect_equal(object = result$mode, expected = "absorbance")
-  
+
   testthat::expect_equal(object = result$units, expected = "?")
-  
+
   testthat::expect_true(result$is.descending)
-  
+
   testthat::expect_equal(object = nrow(result$data),
                          expected = 4827)
 })
@@ -33,15 +33,15 @@ test_that("Read Bruker Opus binary example file with generic read function", {
 test_that("Read Nicolet spa example file with generic read function", {
   path <- soilspec.format::nicolet.spa.file.path()
   result <- soilspec.format::read.soilspec(path)
-  
+
   testthat::expect_equal(object = result$status, expected = 0)
-  
+
   testthat::expect_equal(object = result$mode, expected = "absorbance")
-  
+
   testthat::expect_equal(object = result$units, expected = "?")
-  
+
   testthat::expect_true(result$is.descending)
-  
+
   testthat::expect_equal(object = nrow(result$data),
                          expected = 1971)
 })
@@ -49,17 +49,47 @@ test_that("Read Nicolet spa example file with generic read function", {
 test_that("Read Thermo spc example file with generic read function", {
   path <- soilspec.format::thermo.spc.file.path()
   result <- soilspec.format::read.soilspec(path)
-  
+
   testthat::expect_equal(object = result$status, expected = 0)
-  
+
   testthat::expect_equal(object = result$mode, expected = "?")
-  
+
   testthat::expect_equal(object = result$units, expected = "?")
-  
+
   testthat::expect_true(result$is.descending)
-  
+
   testthat::expect_equal(object = nrow(result$data),
                          expected = 3676)
+})
+
+# Tests for each known type via format specific functions
+
+test_that("Read ASD binary example file with format specific read function", {
+  path <- soilspec.format::asd.binary.file.path()
+  result <- soilspec.format::read.asd.binary(path)
+
+  testthat::expect_equal(object = result$status, expected = 0)
+})
+
+test_that("Read Bruker Opus binary example file with format specific read function", {
+  path <- soilspec.format::bruker.opus.binary.file.path()
+  result <- soilspec.format::read.bruker.opus.binary(path)
+
+  testthat::expect_equal(object = result$status, expected = 0)
+})
+
+test_that("Read Nicolet spa example file with format specific read function", {
+  path <- soilspec.format::nicolet.spa.file.path()
+  result <- soilspec.format::read.nicolet.spa(path)
+
+  testthat::expect_equal(object = result$status, expected = 0)
+})
+
+test_that("Read Thermo spc example file with format specific read function", {
+  path <- soilspec.format::thermo.spc.file.path()
+  result <- soilspec.format::read.thermo.spc(path)
+
+  testthat::expect_equal(object = result$status, expected = 0)
 })
 
 # Tests for unreachable path, unknown file suffix, invalid/unknown file format
@@ -69,9 +99,20 @@ test_that("Attempt to read unreachable path with generic read function", {
   testthat::expect_equal(object = result$status, expected = 1)
 })
 
+test_that("Attempt to read unreachable path with common (internal) read function", {
+  result <- soilspec.format::read.soilspec.common("a/b/c", ".spa")
+  testthat::expect_equal(object = result$status, expected = 1)
+})
+
 test_that("Attempt to read file with unknown file suffix with generic read function", {
   path <- soilspec.format::unknown.file.path()
   result <- soilspec.format::read.soilspec(path)
+  testthat::expect_equal(object = result$status, expected = 2)
+})
+
+test_that("Attempt to read file with unknown file suffix with common (internal) read function", {
+  path <- soilspec.format::bruker.opus.binary.file.path()
+  result <- soilspec.format::read.soilspec.common(path, ".foo")
   testthat::expect_equal(object = result$status, expected = 2)
 })
 
@@ -79,6 +120,12 @@ test_that("Attempt to read Nicolet spa file with invalid format using generic re
   path <- soilspec.format::unknown.file.path()
   path <- stringr::str_replace(path, pattern="xyz", replacement = "spa")
   result <- soilspec.format::read.soilspec(path)
+  testthat::expect_equal(object = result$status, expected = 4)
+})
+
+test_that("Attempt to read Nicolet spa file with invalid format using common (internal) read function", {
+  path <- soilspec.format::bruker.opus.binary.file.path()
+  result <- soilspec.format::read.soilspec.common(path, ".spa")
   testthat::expect_equal(object = result$status, expected = 4)
 })
 
