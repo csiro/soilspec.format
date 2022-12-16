662f314621aae8aa565110c4afae26bbf701eb76
David Benn <David.Benn@csiro.au>
2022-12-16 15:54:44 +1030

    added more checks in spc reader and moved hyperspec reader code to finally clause

--- R/thermo_spc_format.R
+++ R/thermo_spc_format.R
@@ -16,19 +16,25 @@ ThermoSpc <- R6::R6Class("ThermoSpc",
       status <- 4
       mode <- NULL
 
-      out <- tryCatch({
-        spec.data <- hyperSpec::read.spc(path)
-        spec.df <- data.frame(wavenumber=as.double(names(spec.data$spc[,])),
-                              intensity=as.double(spec.data$spc[,]))
-        meta.list <- list()
-        status <- 0
-      },
-      error=function(cond) {
-      },
-      warning=function(cond) {
-      },
-      finally={
-      })
+      if (!file.exists(path)) {
+        status <- 1
+      } else if (file.info(path)$size == 0) {
+        # use defaults
+      } else {
+        out <- tryCatch({
+        },
+        error=function(cond) {
+        },
+        warning=function(cond) {
+        },
+        finally={
+          spec.data <- hyperSpec::read.spc(path)
+          spec.df <- data.frame(wavenumber=as.double(names(spec.data$spc[,])),
+                                intensity=as.double(spec.data$spc[,]))
+          meta.list <- list()
+          status <- 0
+        })
+      }
 
       super$create.result(status, mode, spec.df, meta.list)
     }
