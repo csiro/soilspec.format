014d045a0141f25aa68facc893c12fc5d43427d8
David Benn <David.Benn@csiro.au>
2022-12-16 15:56:25 +1030

    indenting change to sco binary reader

--- R/asd_sco_binary_format.R
+++ R/asd_sco_binary_format.R
@@ -1,45 +1,45 @@
 # ASD SCO binary format, e.g. for vis-NIR contact probe
 
 ASDScoBinary <- R6::R6Class("ASDScoBinary",
-                         inherit = SpectrumFormat,
+  inherit = SpectrumFormat,
 
-                         public = list(
-                           initialize = function() {
-                             super$initialize(origin = "ASD",
-                                              type_name = "visNIR",
-                                              suffix = ".sco")
-                           },
+  public = list(
+   initialize = function() {
+     super$initialize(origin = "ASD",
+                      type_name = "visNIR",
+                      suffix = ".sco")
+   },
 
-                           read = function(path) {
-                             spec.data <- NULL
-                             spec.df <- NULL
-                             meta.list <- NULL
-                             status <- 4
-                             mode <- NULL
+   read = function(path) {
+     spec.data <- NULL
+     spec.df <- NULL
+     meta.list <- NULL
+     status <- 4
+     mode <- NULL
 
-                             if (!file.exists(path)) {
-                               status <- 1
-                             } else if (file.info(path) != 0) {
-                               out <- tryCatch({
-                               },
-                               error=function(cond) {
-                               },
-                               warning=function(cond) {
-                               },
-                               finally={
-                                 spec.data <- t(prospectr::readASD(path))
+     if (!file.exists(path)) {
+       status <- 1
+     } else if (file.info(path)$size != 0) {
+       out <- tryCatch({
+       },
+       error=function(cond) {
+       },
+       warning=function(cond) {
+       },
+       finally={
+         spec.data <- t(prospectr::readASD(path))
 
-                                 spec.df <-
-                                    data.frame(wavenumber=as.integer(rownames(spec.data)),
-                                               intensity=spec.data[1:length(spec.data)])
+         spec.df <-
+            data.frame(wavenumber=as.integer(rownames(spec.data)),
+                       intensity=spec.data[1:length(spec.data)])
 
-                                 meta.list <- list()
-                                 meta.list[["name"]] <- colnames(spec.data)
-                                 status <- 0
-                               })
-                             }
+         meta.list <- list()
+         meta.list[["name"]] <- colnames(spec.data)
+         status <- 0
+       })
+     }
 
-                             super$create.result(status, mode, spec.df, meta.list)
-                          }
-                         )
+     super$create.result(status, mode, spec.df, meta.list)
+  }
+  )
 )
