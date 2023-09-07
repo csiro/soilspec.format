1aa427172483ef4aa62ab5e342da1d5570a3bfd1
David Benn <David.Benn@csiro.au>
2023-09-07 15:11:38 +0930

    removed unused code from Bruker reader class

--- R/bruker_opus_binary_format.R
+++ R/bruker_opus_binary_format.R
@@ -26,32 +26,6 @@ BrukerOpusBinary <- R6::R6Class("BrukerOpusBinary",
         status <- 4
 
         out <- tryCatch({
-          # metadata via opusreader
-          # spec.data <- opusreader::opus_read(path)
-          # mode <- spec.data$metadata$result_spc
-          # meta.list <- as.list(spec.data$metadata)
-
-          # data via opusreader2
-          # spec2.data <- opusreader2::read_opus_file(path)
-          # spec.df <- data.frame(wavenumber=spec2.data$spec$wavenumbers,
-          #                       intensity=spec2.data$spec$data[1:ncol(spec2.data$spec$data)])
-
-          # opus2 <- opusreader2::read_opus(path)
-          # fname <- names(opus2)[1]
-          # spc <- opus2[[fname]][["sc_sample"]][["data"]]
-          # wavelength_a <- opus2[[fname]][["sc_sample"]][["wavenumbers"]]
-          # spec.df <- data.frame(wavenumber=wavelength_a, intensity=unlist(as.list(spc)))
-          #
-          # status <- 0
-
-          # opus2 <- opusreader2::read_opus(path)
-          # fname <- names(opus2)[1]
-          # sc_sample <- opus2[[fname]][["sc_sample"]]
-          # spc <- sc_sample[["data"]]
-          # wavelength_a <- sc_sample[["wavenumbers"]]
-          # spec.df <- data.frame(wavenumber=wavelength_a, intensity=unlist(as.list(spc)))
-          #
-          # status <- 0
         },
         error=function(cond) {
         },
