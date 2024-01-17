7d8cb48aacb54f5d7c4a227f3414c3e0b7a8f5dd
Searle <sea084@csiro.au>
2024-01-17 16:28:00 +1000

    First draft of parseHoneRedRawFile function

---
+++ R/ParseRawFileForReading.R
@@ -0,0 +1,53 @@
+########################################################################
+#####       Author : Ross Searle                                   #####
+#####       Date :  Wed Jan 17 14:46:34 2024                       #####
+#####       Purpose : Parses a raw spectra file from the Hone      #####
+#####                 Lab Red NIR instrument to seperate out       #####
+#####                 into individual files containing individual  #####
+#####                 SAMPLE and BACKGROUND records for formating  #####
+#####                 in the read.soilspec() function              #####
+#####                                                              #####
+########################################################################
+
+
+parseHoneLabRed <- function(path = path, outDirectory = outDirectory)
+#' @title Parse a raw Hone Lab Red spectra file containing multiple spectra
+#' @description  Parses a raw Hone Lab Red spectra file containing multiple spectra into individual files containing a single spectra for reading in read.soilspec() function
+#' @export
+#' @param path Full path to the raw Hone Lab Red spectra file
+#' @param outDirectory Full path to the output directory
+#' @return A list of file names each containing 1 sample and 1 background record
+
+parseHoneLabRed <- function(path, outDirectory) {
+
+if(!dir.exists(outDirectory)){dir.create(outDirectory)}
+
+  indf <- read.csv(path, header = T, check.names=F)
+
+  SpecSets <- unique(indf$SampleSpectrumSetId)
+
+  for(i in 1:length(SpecSets)){
+    print(paste0('Processing Spectral Set ', i, ' of ', length(SpecSets)))
+    specid <- SpecSets[i]
+    idxs <- which(indf$SampleSpectrumSetId==specid)
+    specdf <- indf[idxs,]
+
+    #####  Need to ask the experts what to do when there is more than one background record
+    idxB <- which(specdf$Type=='BACKGROUND')
+    bkgDF <- specdf[idxB,]
+
+    idxS <-  which(specdf$Type=='SAMPLE')
+    sampDF <- specdf[idxS,]
+
+    for (j in 1:nrow(sampDF)) {
+      srec = sampDF[j,]
+      brec = specdf[1,]
+
+      fname = paste0(srec$'Sample ID', '_', srec$SampleSpectrumSetId, '_', srec$SpectrumId)
+      odf <- rbind(brec, srec)
+      write.csv(odf, paste0(outDirectory, '/', fname, '.hlr'), row.names = F)
+
+      }
+  }
+
+}
---
+++ inst/Dev/Hone Dev.R
@@ -0,0 +1,47 @@
+library(stringr)
+library(soilspec.format)
+
+
+adf <- read.csv('C:/Projects/Spectra/Hone - ACIAR Pacific Soils_20231121_083528__001.csv')
+colnames(adf)
+
+
+remotes::install_github("pierreroudier/opusreader")
+remotes::install_github("spectral-cockpit/opusreader2")
+
+bruker.opus.binary.file.path()
+
+pths <- c(hone.lab.red.file.path(),
+          asd.binary.file.path(),
+          asd.sco.binary.file.path (),
+          bruker.opus.binary.file.path(),
+          nicolet.spa.file.path(),
+          thermo.spc.file.path(),
+          perkin.elmer.sp.pepe.file.path(),
+          perkin.elmer.sp.peir.file.path()
+          )
+
+outDir <- 'C:/Temp/SpectraMeta'
+for (i in 1:length(pths)) {
+
+  p <- pths[i]
+  spec <- read.soilspec(p)
+  str(spec)
+  #ext <- strsplit(basename(p), split="\\.")[[1]][2]
+  #fname <- str_remove(basename(p), paste0('.', ext))
+  fname <- basename(dirname(p))
+  m <- spec$metadata
+  metaDf <- data.frame(tag=m, value=as.list(m))
+  write.csv(t(as.data.frame(m)), paste0(outDir, '/', fname, '.csv'), row.names = T)
+}
+
+library(opusreader2)
+asd <- read.soilspec(asd.binary.file.path())
+bruckerBinary <- read.soilspec(bruker.opus.binary.file.path())
+hone <- read.soilspec(hone.lab.red.file.path())
+
+path <- 'C:/Projects/Spectra/Hone_Test_1.csv'
+path <- 'C:/Users/sea084/AppData/Local/R/win-library/4.3/soilspec.format/extdata/HoneLabRed/example.hlr'
+path <- hone.lab.red.file.path()
+file.exists(path)
+
